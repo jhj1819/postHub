bce2e8f18ef0ae6fd712c19c15843e2da7e40561
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-06-30 15:02:03 +0900

    Initial Commit

---
+++ src/main/java/study/posthub/PostHubApplication.java
@@ -0,0 +1,13 @@
+package study.posthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostHubApplication.class, args);
+    }
+
+}
