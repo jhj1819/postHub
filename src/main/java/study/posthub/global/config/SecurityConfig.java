21dbeb06151ae271706059da5135a5511d8a685a
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-07-12 17:28:38 +0900

    FEAT: Integrate CustomOAuth2MemberService with OAuth2 login

--- src/main/java/study/posthub/global/config/SecurityConfig.java
+++ src/main/java/study/posthub/global/config/SecurityConfig.java
@@ -8,18 +8,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import study.posthub.domain.member.service.CustomOAuth2MemberService;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2MemberService customOAuth2MemberService;
+
+    public  SecurityConfig(CustomOAuth2MemberService customOAuth2MemberService){
+
+        this.customOAuth2MemberService = customOAuth2MemberService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login((outh2) -> outh2
+                        .userInfoEndpoint((userInfoEndpointConfig) ->
+                                userInfoEndpointConfig.userService(customOAuth2MemberService)))
                 .authorizeHttpRequests(req -> req
                         .anyRequest().authenticated())
                 .build();
