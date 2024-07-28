24c110824cff2e22fdc5ec10088a203af2dfa44d
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-07-28 15:01:24 +0900

    add: CommentApiControllerTest, CommentServiceTest

---
+++ src/test/java/study/posthub/domain/comment/service/CommentServiceTest.java
@@ -0,0 +1,30 @@
+package study.posthub.domain.comment.service;
+
+import org.junit.jupiter.api.Test;
+
+class CommentServiceTest {
+
+    @Test
+    void saveComment() {
+    }
+
+    @Test
+    void saveReply() {
+    }
+
+    @Test
+    void deleteComment() {
+    }
+
+    @Test
+    void getCommentById() {
+    }
+
+    @Test
+    void getCommentsByPostId() {
+    }
+
+    @Test
+    void getCommentsByParentId() {
+    }
+}
