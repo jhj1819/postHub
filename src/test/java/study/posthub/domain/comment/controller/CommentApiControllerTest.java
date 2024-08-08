2b836d26ff726e5ad395984f3d1fa466da757f3d
khyu2 <66515313+Krockseed@users.noreply.github.com>
2024-08-09 02:53:00 +0900

    Test: 주석 처리

--- src/test/java/study/posthub/domain/comment/controller/CommentApiControllerTest.java
+++ src/test/java/study/posthub/domain/comment/controller/CommentApiControllerTest.java
@@ -1,14 +1,14 @@
-package study.posthub.domain.comment.controller;
-
-import org.junit.jupiter.api.Test;
-
-class CommentApiControllerTest {
-
-    @Test
-    void saveComment() {
-    }
-
-    @Test
-    void saveReply() {
-    }
-}
+//package study.posthub.domain.comment.controller;
+//
+//import org.junit.jupiter.api.Test;
+//
+//class CommentApiControllerTest {
+//
+//    @Test
+//    void saveComment() {
+//    }
+//
+//    @Test
+//    void saveReply() {
+//    }
+//}
